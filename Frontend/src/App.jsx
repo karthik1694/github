da21a7ac133f4e6a04cfaf6224eb2733f1424420
Karthik <karthikprem1694@gmail.com>
2024-07-08 22:34:10 +0530

    UI DESIGN COMPLETED

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import ExplorePage from './pages/ExplorePage'
+import LikesPage from './pages/LikesPage'
+import HomePage from './pages/HomePage'
+import Sidebar from './components/Sidebar'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+const App = () => {
+  return (
+    <div className='flex text-white'>
+        <Sidebar/>
+        <div className='max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1'>
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/signup' element={<SignUpPage />} />
+                <Route path='/explore' element={<ExplorePage />} />
+                <Route path='/likes' element={<LikesPage />} />
+            </Routes>
+            <footer>footer</footer>
+        </div>
+    </div>
+  )
+}
+
+export default App

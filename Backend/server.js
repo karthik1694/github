d502dadb21c7aa4e161bc5bc2e4d25beda461921
Karthik <karthikprem1694@gmail.com>
2024-07-15 14:21:51 +0530

    index.html in backend

--- Backend/server.js
+++ Backend/server.js
@@ -32,8 +32,8 @@ app.use("/api/users",userRoutes);
 app.use("/api/explore",exploreRoutes);
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(process.cwd(),'Frontend','dist','index.html'));  // replace with your own path!  // this path should be relative to the project root directory, not the current working directory.  // Make sure your client-side app is set up to serve this file.  // You may need to install serve-static npm package if you haven't already.  // npm install serve-static --save-dev  // then in your server.js file, import and use it like this:  // import serveStatic from'serve-static';  // const serve = serveStatic(path.join(__dirname, 'frontend/dist'));  // app.use(serve);  // app.get('*', (req, res) => {  //   res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));  // });  // this line of code should be at the end
-})
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.listen(5000,()=>{
